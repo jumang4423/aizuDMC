1268fd263602d03173c66d483b7b468612d41815
ryoma okuda <catontheskate4423@gmail.com>
2021-03-22 05:24:46 +0900

    init

--- src/components/HeaderFoot.tsx
+++ src/components/HeaderFoot.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Link } from "gatsby"
-// import { useLocation } from "@reach/router"
-import { motion } from "framer-motion";
 import "@/styles/component/HeaderFoot.scss";
 
 interface Props {
--- src/layouts/MainPage.tsx
+++ src/layouts/MainPage.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useLocation } from "@reach/router"
 import Main from "@/components/Main"
-import "@/styles/layout/MainPage.scss";
 
 //basically this <Apps /> for only jsx components
 const MainPage: React.FC<any> = () => {
